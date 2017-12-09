17b70964482d86f1d6c9887b7b1a2467f26199cd
Deepi32 <Deepi@192.168.1.3>
2017-12-09 13:19:35 +0530

    First check in for deisgn

---
+++ DesignPattern/src/SingletonDesign.java
@@ -0,0 +1,57 @@
+/*class A
+{
+	public static A instance=new A();
+	private A()
+	{
+		System.out.println("Eager initialization");
+	}
+}
+
+*/
+/*
+class A
+{
+	public static A instance;
+	private A()
+	{
+		System.out.println("using static block");
+	}
+	static{//CPU time wasted 
+		System.out.println("static block");
+		instance=new A();
+	}
+}*/
+class A implements Runnable
+{
+	public static A instance=null;
+	private A()
+	{
+		System.out.println("using static block");
+	}
+	public static A getInstance()
+	{
+		if(instance==null){
+			System.out.println("new instance created");
+			instance=new A();}
+		return instance;
+		
+	}
+	public void run()
+	{
+		System.out.println("");
+		getInstance();
+	}
+}
+public class SingletonDesign {
+	public static void main(String args[])
+	{
+		A a=A.getInstance();
+		Thread t1=new Thread(a);
+		A b=A.getInstance();
+		Thread t2=new Thread(b);
+		t1.start();
+		t2.start();
+		
+	}
+
+}
